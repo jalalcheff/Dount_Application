d91d1d5ac496871fbdd0c2667c87f91d4dcec26c
jalal cheff <jlalalmyahy571@gmail.com>
2023-07-21 20:14:42 +0300

    add animation to inboarding

--- app/src/main/java/com/example/dountapplication/MainActivity.kt
+++ app/src/main/java/com/example/dountapplication/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.dountapplication.screen.DonutsInboarding
 import com.example.dountapplication.screen.DonutsScreen
 import com.example.dountapplication.ui.theme.DountApplicationTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,7 +20,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DonutsScreen()
+            DonutsInboarding()
         }
     }
 }
--- app/src/main/java/com/example/dountapplication/screen/DonutsInbording.kt
+++ app/src/main/java/com/example/dountapplication/screen/DonutsInbording.kt
@@ -1,30 +1,31 @@
 package com.example.dountapplication.screen
 
+import androidx.compose.animation.core.InfiniteRepeatableSpec
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.animateValue
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -41,6 +42,12 @@ fun DonutsInboarding(){
             .fillMaxSize()
             .background(backgroundPink)
     ) {
+        val infiniteTransition = rememberInfiniteTransition()
+        val donutsAnimate by infiniteTransition.animateFloat(
+            targetValue = -50f,
+            initialValue = 20f,
+            animationSpec = InfiniteRepeatableSpec(tween(6000), repeatMode = RepeatMode.Reverse)
+        )
         Image(
             painter = painterResource(id = R.drawable.img),
             contentDescription = "background",
@@ -48,6 +55,7 @@ fun DonutsInboarding(){
                 .fillMaxWidth()
                 .scale(1.6f)
                 .padding(top = 52.dp)
+                .offset(y = donutsAnimate.dp, x = donutsAnimate.dp)
             )
             Text(
                 text = "Gonuts\nwith\nDonuts",
@@ -75,7 +83,8 @@ fun DonutsInboarding(){
             onClick = { /*TODO*/ },
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(horizontal = 16.dp).padding(bottom = 24.dp),
+                .padding(horizontal = 16.dp)
+                .padding(bottom = 24.dp),
             colors = ButtonDefaults.buttonColors(Color.White)
             ) {
             Text(
