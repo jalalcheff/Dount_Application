3349d6251dc9fa90dc2f1281a1484adbffdac8e6
jalal cheff <jlalalmyahy571@gmail.com>
2023-07-21 23:34:26 +0300

    finish bottom bar

--- app/src/main/java/com/example/dountapplication/screen/DonutsHomeScreen.kt
+++ app/src/main/java/com/example/dountapplication/screen/DonutsHomeScreen.kt
@@ -1,20 +1,27 @@
 package com.example.dountapplication.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -32,6 +39,7 @@ import com.example.dountapplication.composable.TodayOfferDonutsCard
 import com.example.dountapplication.composable.VerticalSpacer
 import com.example.dountapplication.viewModel.HomeScreenUiState
 import com.example.dountapplication.viewModel.HomeScreenViewModel
+import com.teckiti.R
 
 @Composable
 fun DonutsHomeScreen(
@@ -48,7 +56,7 @@ fun DonutsHomeContent(state: HomeScreenUiState, onClickDonuts: () -> Unit) {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.White)
+            .background(Color.White).padding(bottom = 16.dp)
     ) {
         Spacer(modifier = Modifier.size(16.dp))
         SearchRow()
@@ -67,6 +75,21 @@ fun DonutsHomeContent(state: HomeScreenUiState, onClickDonuts: () -> Unit) {
         LazyRow(contentPadding = PaddingValues(8.dp), modifier = Modifier.zIndex(1f)){
             items(state.donuts) {
                 DonutsCard(it)
+            }
+        }
+        Spacer(modifier = Modifier.weight(1f))
+        Card(modifier = Modifier.fillMaxWidth(),
+            colors = CardDefaults.cardColors(Color.Transparent)
+            ) {
+            Row(
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Image(painter = painterResource(id = R.drawable.home), contentDescription = "bottomBar")
+                Image(painter = painterResource(id = R.drawable.heart), contentDescription = "bottomBar")
+                Image(painter = painterResource(id = R.drawable.notification), contentDescription = "bottomBar")
+                Image(painter = painterResource(id = R.drawable.buy), contentDescription = "bottomBar")
+                Image(painter = painterResource(id = R.drawable.group), contentDescription = "bottomBar")
             }
         }
     }
