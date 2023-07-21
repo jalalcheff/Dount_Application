1f61e926cfa9980ac3ad056f9576a5c7098a51bd
jalal cheff <jlalalmyahy571@gmail.com>
2023-07-21 23:36:43 +0300

    finish the project

--- app/src/main/java/com/example/dountapplication/util/HomeScreenFakeData.kt
+++ app/src/main/java/com/example/dountapplication/util/HomeScreenFakeData.kt
@@ -19,6 +19,34 @@ object HomeScreenFakeData {
             todayImageResource = R.drawable.today_image2,
             todayDiscountPrice = 16,
             todayOriginalPrice = 20,
+        ),
+        HomeScreenTodayModel(
+            todayTitle = "Strawberry",
+            todayDescription = "These Baked Strawberry Donuts are filled with fresh strawberries...",
+            todayImageResource = R.drawable.today_image1,
+            todayDiscountPrice = 16,
+            todayOriginalPrice = 20,
+        ),
+        HomeScreenTodayModel(
+            todayTitle = "Chocolate",
+            todayDescription = "Moist and fluffy baked chocolate donuts full of chocolate flavor.",
+            todayImageResource = R.drawable.today_image2,
+            todayDiscountPrice = 16,
+            todayOriginalPrice = 20,
+        ),
+        HomeScreenTodayModel(
+            todayTitle = "Strawberry",
+            todayDescription = "These Baked Strawberry Donuts are filled with fresh strawberries...",
+            todayImageResource = R.drawable.today_image1,
+            todayDiscountPrice = 16,
+            todayOriginalPrice = 20,
+        ),
+        HomeScreenTodayModel(
+            todayTitle = "Chocolate",
+            todayDescription = "Moist and fluffy baked chocolate donuts full of chocolate flavor.",
+            todayImageResource = R.drawable.today_image2,
+            todayDiscountPrice = 16,
+            todayOriginalPrice = 20,
         )
     )
     val listOfHomeScreenDonutsFakeData = listOf(
@@ -38,5 +66,21 @@ object HomeScreenFakeData {
             donutsTitle = "Strawberry Coco",
             donutsPrice = 12,
             ),
+        HomeScreenDonutsModel(
+            donutsImageResource = R.drawable.home_donuts1,
+            donutsTitle = "Chocolate Cherry",
+            donutsPrice = 15,
+
+            ),
+        HomeScreenDonutsModel(
+            donutsImageResource = R.drawable.home_donuts2,
+            donutsTitle = "Strawberry Rain",
+            donutsPrice = 13,
+        ),
+        HomeScreenDonutsModel(
+            donutsImageResource = R.drawable.home_donuts3,
+            donutsTitle = "Strawberry Coco",
+            donutsPrice = 20,
+        ),
     )
 }
